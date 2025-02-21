114cd434d66cd0249c160eb3985a5d402b2ac79d
Weixuan Ning <73421965+WeixuanPlant@users.noreply.github.com>
2025-02-21 16:43:10 -0600

    Add files via upload

---
+++ 01_OutgroupSelection_n51/lea.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=100G 
+#SBATCH --time=1-00:00:00
+#SBATCH --mail-user=weixuan@iastate.edu
+#SBATCH --mail-type=ALL
+#SBATCH --open-mode=append
+#SBATCH --output="job.vcf_n90.%J.out"
+#SBATCH --job-name="jointGeno_n90"
+
+
+module load r
+
+Rscript LEA.R
+
+cut -d ' ' -f 2 *.ped  > samplename.txt
